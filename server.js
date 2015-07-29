c15e14ee49f7954e5259a936819ad6ff2e27801c
Derek Wene <dwene@spredfast.com>
2015-07-29 12:15:26 -0500

    Your grunt build is messed up

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('dist'));
+// app.use(express.static('dist'));
 
+app.use('/', express.static('dist'));
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
